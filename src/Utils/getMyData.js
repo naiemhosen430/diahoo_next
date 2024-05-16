091423a3639a76fd704a233b77c89b823d6edae3
naiemhosen430 <naiemhosen430@gmail.com>
2024-05-16 09:55:20 +0600

    d

--- src/Utils/getMyData.js
+++ src/Utils/getMyData.js
@@ -5,7 +5,7 @@ export const getMyData = () => {
     // checking authentication 
     const cookieStore = cookies();
     const accessToken = cookieStore.get("accesstoken")?.value || null;
-    const userInfo = jwt.decode(JSON.parse(accessToken), process.env.TOKEN_SECRET)
+    const userInfo = jwt.decode(accessToken, process.env.TOKEN_SECRET)
 
     if (!userInfo){
         return Response.json({ message:"unauthorized!" }, { status: 498 })
