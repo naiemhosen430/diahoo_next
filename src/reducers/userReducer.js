d55d36d48255e5461dbd5db2f64cd383c61ae06c
naiemhosen430 <naiemhosen430@gmail.com>
2024-05-16 10:04:05 +0600

    d

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -3,6 +3,8 @@ import { userAction } from "@/actions/userAction";
 export const authReducer = (state, action) => {
     switch (action.type) {
       case userAction.addMyData:
+        console.log(action.payload)
+        console.log(action)
         return { ...state, user: action.payload };
       case userAction.deleteMyData:
         return { ...state, user: action.payload };
