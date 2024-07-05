163ab1bd4a17b2f94ce496dfc53eeb5502a7c355
naiemhosen430 <naiemhosen430@gmail.com>
2024-07-05 22:15:04 +0600

    d

--- src/app/myprofile/page.jsx
+++ src/app/myprofile/page.jsx
@@ -1,38 +1,35 @@
-'use client'
-import React, { useEffect, useState } from "react";
-import ProfileHeader from "../Components/CommonComponents/ProfileHeader";
-import ProfileBody from "./Components/ProfileBody";
-import UseAuthContext from "@/Hooks/UseAuthContext";
-import UseGetProfile from "@/Hooks/Profile/UseGetProfile";
+"use client";
+
 
 function page() {
-  const { user } = UseAuthContext();
-    const { profile, loading, error, getProfile } = UseGetProfile(); 
-    console.log(profile)
+  // const user = null;
+  // const { profile, loading, error, getProfile } = UseGetProfile();
+  // console.log(profile);
 
-    useEffect(() => {
-        if (user) {
-            getProfile(user.id); 
-        }
-    }, [user]);
+  // useEffect(() => {
+  //   if (user) {
+  //     getProfile(user.id);
+  //   }
+  // }, [user]);
 
-    if (loading || !profile) {
-        return <h1 className="text-white text-center">Loading....</h1>; 
-    }
+  // if (loading || !profile) {
+  //   return <h1 className="text-white text-center">Loading....</h1>;
+  // }
 
-    if (error) {
-        return <h1 className="text-white text-center">Error: {error}</h1>; 
-    }
+  // if (error) {
+  //   return <h1 className="text-white text-center">Error: {error}</h1>;
+  // }
   return (
     <div className=" h-screen overflow-y-auto custom-scrollbar-hidden bg-slate-950 mx-2">
-      <ProfileHeader
+      {/* <ProfileHeader
         coverImage={profile?.coverImage}
         profileImage={profile?.profileImage}
         fullName={profile?.fullName}
         tittle={profile?.tittle}
       />
 
-      <ProfileBody />
+      <ProfileBody /> */}
+      hello
     </div>
   );
 }
